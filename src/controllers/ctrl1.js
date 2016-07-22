bda689e256933755a999d5436045b17629845adc
Karol Zawadzki <karol@pierwiastek.org>
2016-07-23 01:03:06 +0200

    super hot fix

--- src/controllers/ctrl1.js
+++ src/controllers/ctrl1.js
@@ -3,7 +3,7 @@ var angular = require('angular');
 require('../services/srv1.js');
 
 angular.module('test').controller('testCtrl', ['$scope', 'testSrv', function($scope, testSrv) {
-    $scope.test = 'ytu   ta s u';
+    $scope.test = 'ytu   ta af sadf a fs u';
     $scope.akcja = function() { 
         $scope.test = testSrv.akcja();
     };
